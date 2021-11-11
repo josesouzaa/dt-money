a8519a559ed82cb0b5b333f4ff6d8543b39b6cfe
José de Souza <josee.souzaaa@gmail.com>
2021-11-10 21:36:59 -0300

    Criação do hook useTransactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
-import { FormEvent, useState, useContext } from 'react'
-import { TransactionsContext } from '../../TransactionsContext'
+import { FormEvent, useState } from 'react'
+import { useTransactions } from '../../hooks/useTransactions'
 
 import Modal from 'react-modal'
 
@@ -18,7 +18,7 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose
 }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext)
+  const { createTransaction } = useTransactions()
 
   const [title, setTitle] = useState('')
   const [amount, setAmount] = useState(0)
